f7b5cfcf828973d707fd177b045ab50c2ca6f0dd
Maciej Caderek <maciej.caderek@gmail.com>
2024-07-18 21:28:11 +0200

    Rebuilt

--- icons/icons.ts
+++ icons/icons.ts
@@ -1,249 +1,273 @@
 export type IconsId =
-  | "accuracy"
-  | "annotations"
-  | "arrow-down"
-  | "arrow-left"
-  | "arrow-right"
-  | "arrow-up"
-  | "back"
-  | "backspace-word"
-  | "backspace"
-  | "bookmark-off"
-  | "bookmark-on"
-  | "books"
-  | "close"
-  | "dark-mode"
-  | "download"
-  | "enter-1"
-  | "enter"
-  | "extra"
-  | "favorite-off"
-  | "favorite"
-  | "fullscreen-off"
-  | "fullscreen"
-  | "heart-off"
-  | "heart"
-  | "home"
-  | "info"
-  | "less"
-  | "light-mode"
-  | "loading"
-  | "logo"
-  | "menu"
-  | "meta-linux"
-  | "meta-macos"
-  | "meta-windows"
-  | "more"
-  | "new-line"
-  | "next"
-  | "option"
-  | "pause"
-  | "pb"
-  | "prev"
-  | "profile"
-  | "reading-off"
-  | "reading-on"
-  | "reload"
-  | "repeat"
-  | "reset"
-  | "save"
-  | "settings"
-  | "shift-off"
-  | "shift"
-  | "skip"
-  | "speed"
-  | "stats"
-  | "symbols"
-  | "toggle-off"
-  | "toggle-on"
-  | "warning"
+  | "zen"
   | "watch"
-  | "zen";
+  | "warning"
+  | "toggle-on"
+  | "toggle-off"
+  | "symbols"
+  | "stats"
+  | "star"
+  | "star-off"
+  | "star-half"
+  | "speed"
+  | "skip"
+  | "shift"
+  | "shift-off"
+  | "settings"
+  | "save"
+  | "reset"
+  | "repeat"
+  | "reload"
+  | "reading-on"
+  | "reading-off"
+  | "profile"
+  | "prev"
+  | "pb"
+  | "pause"
+  | "option"
+  | "ok"
+  | "next"
+  | "new-line"
+  | "more"
+  | "meta-windows"
+  | "meta-macos"
+  | "meta-linux"
+  | "menu"
+  | "logo"
+  | "loading"
+  | "link-external"
+  | "light-mode"
+  | "less"
+  | "info"
+  | "home"
+  | "heart"
+  | "heart-off"
+  | "fullscreen"
+  | "fullscreen-off"
+  | "flame"
+  | "flame-off"
+  | "flame-half"
+  | "extra"
+  | "enter"
+  | "enter-1"
+  | "download"
+  | "dark-mode"
+  | "close"
+  | "books"
+  | "bookmark-on"
+  | "bookmark-off"
+  | "backspace"
+  | "backspace-word"
+  | "back"
+  | "arrow-up"
+  | "arrow-right"
+  | "arrow-left"
+  | "arrow-down"
+  | "annotations"
+  | "accuracy";
 
 export type IconsKey =
-  | "Accuracy"
-  | "Annotations"
-  | "ArrowDown"
-  | "ArrowLeft"
-  | "ArrowRight"
-  | "ArrowUp"
-  | "Back"
-  | "BackspaceWord"
-  | "Backspace"
-  | "BookmarkOff"
-  | "BookmarkOn"
-  | "Books"
-  | "Close"
-  | "DarkMode"
-  | "Download"
-  | "Enter_1"
-  | "Enter"
-  | "Extra"
-  | "FavoriteOff"
-  | "Favorite"
-  | "FullscreenOff"
-  | "Fullscreen"
-  | "HeartOff"
-  | "Heart"
-  | "Home"
-  | "Info"
-  | "Less"
-  | "LightMode"
-  | "Loading"
-  | "Logo"
-  | "Menu"
-  | "MetaLinux"
-  | "MetaMacos"
-  | "MetaWindows"
-  | "More"
-  | "NewLine"
-  | "Next"
-  | "Option"
-  | "Pause"
-  | "Pb"
-  | "Prev"
-  | "Profile"
-  | "ReadingOff"
-  | "ReadingOn"
-  | "Reload"
-  | "Repeat"
-  | "Reset"
-  | "Save"
-  | "Settings"
-  | "ShiftOff"
-  | "Shift"
-  | "Skip"
-  | "Speed"
-  | "Stats"
-  | "Symbols"
-  | "ToggleOff"
-  | "ToggleOn"
-  | "Warning"
+  | "Zen"
   | "Watch"
-  | "Zen";
+  | "Warning"
+  | "ToggleOn"
+  | "ToggleOff"
+  | "Symbols"
+  | "Stats"
+  | "Star"
+  | "StarOff"
+  | "StarHalf"
+  | "Speed"
+  | "Skip"
+  | "Shift"
+  | "ShiftOff"
+  | "Settings"
+  | "Save"
+  | "Reset"
+  | "Repeat"
+  | "Reload"
+  | "ReadingOn"
+  | "ReadingOff"
+  | "Profile"
+  | "Prev"
+  | "Pb"
+  | "Pause"
+  | "Option"
+  | "Ok"
+  | "Next"
+  | "NewLine"
+  | "More"
+  | "MetaWindows"
+  | "MetaMacos"
+  | "MetaLinux"
+  | "Menu"
+  | "Logo"
+  | "Loading"
+  | "LinkExternal"
+  | "LightMode"
+  | "Less"
+  | "Info"
+  | "Home"
+  | "Heart"
+  | "HeartOff"
+  | "Fullscreen"
+  | "FullscreenOff"
+  | "Flame"
+  | "FlameOff"
+  | "FlameHalf"
+  | "Extra"
+  | "Enter"
+  | "Enter1"
+  | "Download"
+  | "DarkMode"
+  | "Close"
+  | "Books"
+  | "BookmarkOn"
+  | "BookmarkOff"
+  | "Backspace"
+  | "BackspaceWord"
+  | "Back"
+  | "ArrowUp"
+  | "ArrowRight"
+  | "ArrowLeft"
+  | "ArrowDown"
+  | "Annotations"
+  | "Accuracy";
 
 export enum Icons {
-  Accuracy = "accuracy",
-  Annotations = "annotations",
-  ArrowDown = "arrow-down",
-  ArrowLeft = "arrow-left",
-  ArrowRight = "arrow-right",
-  ArrowUp = "arrow-up",
-  Back = "back",
-  BackspaceWord = "backspace-word",
-  Backspace = "backspace",
-  BookmarkOff = "bookmark-off",
-  BookmarkOn = "bookmark-on",
-  Books = "books",
-  Close = "close",
-  DarkMode = "dark-mode",
-  Download = "download",
-  Enter_1 = "enter-1",
-  Enter = "enter",
-  Extra = "extra",
-  FavoriteOff = "favorite-off",
-  Favorite = "favorite",
-  FullscreenOff = "fullscreen-off",
-  Fullscreen = "fullscreen",
-  HeartOff = "heart-off",
-  Heart = "heart",
-  Home = "home",
-  Info = "info",
-  Less = "less",
-  LightMode = "light-mode",
-  Loading = "loading",
-  Logo = "logo",
-  Menu = "menu",
-  MetaLinux = "meta-linux",
-  MetaMacos = "meta-macos",
-  MetaWindows = "meta-windows",
-  More = "more",
-  NewLine = "new-line",
-  Next = "next",
-  Option = "option",
-  Pause = "pause",
-  Pb = "pb",
-  Prev = "prev",
-  Profile = "profile",
-  ReadingOff = "reading-off",
-  ReadingOn = "reading-on",
-  Reload = "reload",
-  Repeat = "repeat",
-  Reset = "reset",
-  Save = "save",
-  Settings = "settings",
-  ShiftOff = "shift-off",
-  Shift = "shift",
-  Skip = "skip",
-  Speed = "speed",
-  Stats = "stats",
-  Symbols = "symbols",
-  ToggleOff = "toggle-off",
-  ToggleOn = "toggle-on",
-  Warning = "warning",
-  Watch = "watch",
   Zen = "zen",
+  Watch = "watch",
+  Warning = "warning",
+  ToggleOn = "toggle-on",
+  ToggleOff = "toggle-off",
+  Symbols = "symbols",
+  Stats = "stats",
+  Star = "star",
+  StarOff = "star-off",
+  StarHalf = "star-half",
+  Speed = "speed",
+  Skip = "skip",
+  Shift = "shift",
+  ShiftOff = "shift-off",
+  Settings = "settings",
+  Save = "save",
+  Reset = "reset",
+  Repeat = "repeat",
+  Reload = "reload",
+  ReadingOn = "reading-on",
+  ReadingOff = "reading-off",
+  Profile = "profile",
+  Prev = "prev",
+  Pb = "pb",
+  Pause = "pause",
+  Option = "option",
+  Ok = "ok",
+  Next = "next",
+  NewLine = "new-line",
+  More = "more",
+  MetaWindows = "meta-windows",
+  MetaMacos = "meta-macos",
+  MetaLinux = "meta-linux",
+  Menu = "menu",
+  Logo = "logo",
+  Loading = "loading",
+  LinkExternal = "link-external",
+  LightMode = "light-mode",
+  Less = "less",
+  Info = "info",
+  Home = "home",
+  Heart = "heart",
+  HeartOff = "heart-off",
+  Fullscreen = "fullscreen",
+  FullscreenOff = "fullscreen-off",
+  Flame = "flame",
+  FlameOff = "flame-off",
+  FlameHalf = "flame-half",
+  Extra = "extra",
+  Enter = "enter",
+  Enter1 = "enter-1",
+  Download = "download",
+  DarkMode = "dark-mode",
+  Close = "close",
+  Books = "books",
+  BookmarkOn = "bookmark-on",
+  BookmarkOff = "bookmark-off",
+  Backspace = "backspace",
+  BackspaceWord = "backspace-word",
+  Back = "back",
+  ArrowUp = "arrow-up",
+  ArrowRight = "arrow-right",
+  ArrowLeft = "arrow-left",
+  ArrowDown = "arrow-down",
+  Annotations = "annotations",
+  Accuracy = "accuracy",
 }
 
 export const ICONS_CODEPOINTS: { [key in Icons]: string } = {
-  [Icons.Accuracy]: "61697",
-  [Icons.Annotations]: "61698",
-  [Icons.ArrowDown]: "61699",
-  [Icons.ArrowLeft]: "61700",
-  [Icons.ArrowRight]: "61701",
-  [Icons.ArrowUp]: "61702",
-  [Icons.Back]: "61703",
-  [Icons.BackspaceWord]: "61704",
-  [Icons.Backspace]: "61705",
-  [Icons.BookmarkOff]: "61706",
-  [Icons.BookmarkOn]: "61707",
-  [Icons.Books]: "61708",
-  [Icons.Close]: "61709",
-  [Icons.DarkMode]: "61710",
-  [Icons.Download]: "61711",
-  [Icons.Enter_1]: "61712",
-  [Icons.Enter]: "61713",
-  [Icons.Extra]: "61714",
-  [Icons.FavoriteOff]: "61715",
-  [Icons.Favorite]: "61716",
-  [Icons.FullscreenOff]: "61717",
-  [Icons.Fullscreen]: "61718",
-  [Icons.HeartOff]: "61719",
-  [Icons.Heart]: "61720",
-  [Icons.Home]: "61721",
-  [Icons.Info]: "61722",
-  [Icons.Less]: "61723",
-  [Icons.LightMode]: "61724",
-  [Icons.Loading]: "9676",
-  [Icons.Logo]: "61725",
-  [Icons.Menu]: "61726",
-  [Icons.MetaLinux]: "61727",
-  [Icons.MetaMacos]: "61728",
-  [Icons.MetaWindows]: "61729",
-  [Icons.More]: "61730",
+  [Icons.Zen]: "61697",
+  [Icons.Watch]: "61698",
+  [Icons.Warning]: "61699",
+  [Icons.ToggleOn]: "61700",
+  [Icons.ToggleOff]: "61701",
+  [Icons.Symbols]: "61702",
+  [Icons.Stats]: "61703",
+  [Icons.Star]: "9733",
+  [Icons.StarOff]: "9734",
+  [Icons.StarHalf]: "11242",
+  [Icons.Speed]: "61704",
+  [Icons.Skip]: "61705",
+  [Icons.Shift]: "61706",
+  [Icons.ShiftOff]: "61707",
+  [Icons.Settings]: "61708",
+  [Icons.Save]: "61709",
+  [Icons.Reset]: "61710",
+  [Icons.Repeat]: "61711",
+  [Icons.Reload]: "61712",
+  [Icons.ReadingOn]: "61713",
+  [Icons.ReadingOff]: "61714",
+  [Icons.Profile]: "61715",
+  [Icons.Prev]: "11164",
+  [Icons.Pb]: "61716",
+  [Icons.Pause]: "61717",
+  [Icons.Option]: "61718",
+  [Icons.Ok]: "10003",
+  [Icons.Next]: "11166",
   [Icons.NewLine]: "9166",
-  [Icons.Next]: "61731",
-  [Icons.Option]: "61732",
-  [Icons.Pause]: "61733",
-  [Icons.Pb]: "61734",
-  [Icons.Prev]: "61735",
-  [Icons.Profile]: "61736",
-  [Icons.ReadingOff]: "61737",
-  [Icons.ReadingOn]: "61738",
-  [Icons.Reload]: "61739",
-  [Icons.Repeat]: "61740",
-  [Icons.Reset]: "61741",
-  [Icons.Save]: "61742",
-  [Icons.Settings]: "61743",
-  [Icons.ShiftOff]: "61744",
-  [Icons.Shift]: "61745",
-  [Icons.Skip]: "61746",
-  [Icons.Speed]: "61747",
-  [Icons.Stats]: "61748",
-  [Icons.Symbols]: "61749",
-  [Icons.ToggleOff]: "61750",
-  [Icons.ToggleOn]: "61751",
-  [Icons.Warning]: "61752",
-  [Icons.Watch]: "61753",
-  [Icons.Zen]: "61754",
+  [Icons.More]: "11167",
+  [Icons.MetaWindows]: "61719",
+  [Icons.MetaMacos]: "61720",
+  [Icons.MetaLinux]: "61721",
+  [Icons.Menu]: "61722",
+  [Icons.Logo]: "61723",
+  [Icons.Loading]: "9676",
+  [Icons.LinkExternal]: "10150",
+  [Icons.LightMode]: "61724",
+  [Icons.Less]: "11165",
+  [Icons.Info]: "61725",
+  [Icons.Home]: "61726",
+  [Icons.Heart]: "61727",
+  [Icons.HeartOff]: "61728",
+  [Icons.Fullscreen]: "61729",
+  [Icons.FullscreenOff]: "61730",
+  [Icons.Flame]: "9670",
+  [Icons.FlameOff]: "9671",
+  [Icons.FlameHalf]: "11030",
+  [Icons.Extra]: "61731",
+  [Icons.Enter]: "61732",
+  [Icons.Enter1]: "61733",
+  [Icons.Download]: "61734",
+  [Icons.DarkMode]: "61735",
+  [Icons.Close]: "61736",
+  [Icons.Books]: "61737",
+  [Icons.BookmarkOn]: "61738",
+  [Icons.BookmarkOff]: "61739",
+  [Icons.Backspace]: "61740",
+  [Icons.BackspaceWord]: "61741",
+  [Icons.Back]: "61742",
+  [Icons.ArrowUp]: "61743",
+  [Icons.ArrowRight]: "61744",
+  [Icons.ArrowLeft]: "61745",
+  [Icons.ArrowDown]: "61746",
+  [Icons.Annotations]: "61747",
+  [Icons.Accuracy]: "61748",
 };
